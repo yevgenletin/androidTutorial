8a6ed856c7266910032fd6df5c5545d5944d04d0
yevgenletin <yevgenletin@gmail.com>
2017-03-22 22:27:04 +0200

    Create project

---
+++ app/src/main/java/com/example/yevhen1992/p0501_simpleadaptercustom2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yevhen1992.p0501_simpleadaptercustom2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
